5f8d5bb50bfc0e0bc1e75ce2db48b6e45834fab3
Thembinkosi Mahlobo <themba.romano@hotmail.co.uk>
2024-05-13 15:57:10 +0100

    add changes and updates for src

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,27 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <h1>Bromptastic engineering</h1>
+        <h2> Brompton bikes are stylish and sofisticated</h2>
+        <p> sign for a secret ride</p>
+        <SignedOut></SignedOut>
+        <div class=" border-l-pink-400 text-xl">
+          <SignedIn>
+            <a href="https://www.brompton.com/c/electric-bikes/">
+              https://www.brompton.com/c/electric-bikes
+            </a>
+          </SignedIn>
+        </div>
+      </div>
+    </>
+  );
 }
